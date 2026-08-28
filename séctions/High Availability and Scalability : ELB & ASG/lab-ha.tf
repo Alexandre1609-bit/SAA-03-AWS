cf4a6c6b00b2e5209d62c31689d6cf4f309d9e66
Alexandre1609-bit <alexandre.regnier59@live.fr>
2026-08-28 16:40:06 +0200

    chore(lab-ha): add ALB and update ec2 SG

--- séctions/High Availability and Scalability : ELB & ASG/lab-ha.tf
+++ séctions/High Availability and Scalability : ELB & ASG/lab-ha.tf
@@ -43,13 +43,100 @@ resource "aws_launch_template" "test-template" {
     configured = true
   }
 
+  user_data = <<-EOS
+      #!/bin/bash
+      apt-get update
+      apt-get install -y nginx
+      systemctl start nginx
+      systemctl enable nginx
+      EOS
+
   iam_instance_profile {
     name = aws_iam_instance_profile.real_inst_prof.name
   }
 
-  security_group_names = [aws_security_group.allow_tls.name]
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  security_group_names   = [aws_security_group.ec2_sg.name]
 
 }
+
+### ALB ###
+
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "my-alb"
+  vpc_id  = aws_vpc.main_vpc.id
+  subnets = [aws_subnet.subnet_one.id, aws_subnet.subnet_two.id]
+
+  #security_group_name = aws_security_group.allow_tls.name  à laisser et supprimer la partie juste en dessous ou pas ?
+
+  # Security Group
+  security_group_ingress_rules = {
+    all_http = {
+      from_port   = 80
+      to_port     = 80
+      ip_protocol = "tcp"
+      description = "HTTP web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+    all_https = {
+      from_port   = 443
+      to_port     = 443
+      ip_protocol = "tcp"
+      description = "HTTPS web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
+  security_group_egress_rules = {
+    all = {
+      ip_protocol = "-1"
+      cidr_ipv4   = var.cidr_block_vpc_one
+    }
+  }
+
+  #access_logs = {
+  #  bucket = "my-alb-logs"
+  #} Optionnel, se renseigner 
+
+  listeners = {
+    http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+    ex-https = {
+      port            = 443
+      protocol        = "HTTPS"
+      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012" #Certificat à créer, se renseigner afin de le faire dans les prochains lab
+
+      forward = {
+        target_group_key = "inst_1"
+      }
+    }
+  }
+
+  target_groups = {
+    inst_1 = {
+      name_prefix = "h1"
+      protocol    = "HTTP"
+      port        = 80
+      target_type = "instance"
+      target_id   = "i-0f6d38a07d50d080f"
+    }
+  }
+
+  tags = {
+    Environment = "Development"
+    Project     = "Example"
+  }
+}
+
+### TARGET GROUP ###
 
 ### IAM ROLE ###
 
@@ -125,22 +212,16 @@ resource "aws_route_table_association" "assoc_snd" {
 
 ### SECURITY GROUP ###
 
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2_sg"
   description = "Allow TLS inbound traffic and all outbound traffic"
   vpc_id      = aws_vpc.main_vpc.id
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = var.cidr_block_vpc_one
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
-}
-
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # All ports
+resource "aws_vpc_security_group_ingress_rule" "allow_alb_http" {
+  security_group_id            = aws_security_group.ec2_sg.id
+  referenced_security_group_id = module.alb.security_group_id
+  from_port                    = 80
+  to_port                      = 80
+  ip_protocol                  = "TCP"
 }
